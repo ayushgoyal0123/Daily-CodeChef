2d9c8695407091a52212ae502874679139d4260d
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-11-01 18:36:04 +0530

    Add files via upload

---
+++ Day-4 THREETOPICS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+
+	int A,B,C,X;
+	cin>>A>>B>>C>>X;
+	if(X==A||X==B||X==C)
+	    cout<<"Yes";
+	 else
+	    cout<<"No";
+	    
+return 0;
+
+}
+
