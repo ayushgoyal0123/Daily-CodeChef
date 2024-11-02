7532c12aca1fd0eb7a211589a4f1286f37f0540d
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-11-03 02:32:40 +0530

    Add files via upload

---
+++ Day-7 JASSIGNMENTS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i,T,X;
+	cin>>T;
+	for(i=0;i<T;i++)
+	{
+	    cin>>X;
+	    if(X<=7)
+	        cout<<"Yes\n";
+	    else
+	        cout<<"No\n";
+	}
+return 0;
+}
