5fa60120b40234f757be3a1cbe76a09c2f7a6960
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-11-05 21:21:30 +0530

    Add files via upload

---
+++ Day 17 TRAVELFAST.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i,T,X,Y;
+	cin>>T;
+	for(i=0;i<T;i++)
+	{
+	    cin>>X>>Y;
+	    if(X<Y)
+	        cout<<"BIKE\n";
+	        
+	    else if(X>Y)
+	        cout<<"CAR\n";
+	    
+	    else
+	        cout<<"SAME\n";
+	}
+return 0;
+}
