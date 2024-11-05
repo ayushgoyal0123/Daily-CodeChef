a4e3ea3b845843fa60064c4cc31b7600159d2868
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-11-05 13:18:50 +0530

    Add files via upload

---
+++ Day 16 NOTEBOOK.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int i,T,N;
+	cin>>T;
+	for(i=0;i<T;i++)
+	{
+	    cin>>N;
+	    cout<<N*10<<"\n";
+	}
+return 0;
+}
