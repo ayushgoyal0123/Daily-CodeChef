902a0c306a7b5065131e0730d9d8cf443e2e79f2
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-11-12 12:04:27 +0530

    Add files via upload

---
+++ Day 41 BATH.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X,Y;
+	cin>>T;
+	for(int i=0;i<T;i++)
+	{
+	    cin>>X>>Y;
+	    cout<<X/(2*Y)<<"\n";
+	}
+
+}
