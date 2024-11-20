4573925d32a9ca28f56fa6de0a1d0f0d40fdb4db
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-11-21 00:06:19 +0530

    Add files via upload

---
+++ Day 49 OFFICE.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X,Y;
+	cin>>T;
+	for(int i=0;i<T;i++)
+	{
+        cin>>X>>Y;
+        cout<<(4*X)+Y<<"\n";
+	}
+    return 0;
+}
