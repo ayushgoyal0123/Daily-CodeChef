78143e7c674a48ec46fa2ebdfd31f679b92ced56
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-11-24 11:50:13 +0530

    Add files via upload

---
+++ Day 53 DECINC.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int N;
+	cin>>N;
+	if(N%4==0)
+	    cout<<++N;
+	    
+	 else
+	    cout<<--N;
+return 0;
+}
