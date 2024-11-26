4053aa421fd6705bb91c2189465ac7f9aa250601
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-11-26 23:31:53 +0530

    Add files via upload

---
+++ Day 55 CREDCOINS.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T,X,Y;
+	cin>>T;
+	for(int i=1;i<=T;i++)
+	    {
+	        cin>>X>>Y;
+	        cout<<(X*Y)/100<<"\n";
+	    }
+	    
+return 0;
+}
