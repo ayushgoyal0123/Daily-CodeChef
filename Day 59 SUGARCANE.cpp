e599f4c65dc9bda00a450c2bffa1328f64f51c0d
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-12-02 14:37:58 +0530

    Add files via upload

---
+++ Day 59 SUGARCANE.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t,n;
+    cin>>t;
+    for(int i=1;i<=t;i++)
+    {
+        cin>>n;
+        cout<<50*n -20*n - 15*n<<"\n";
+    }
+    return 0;
+}
