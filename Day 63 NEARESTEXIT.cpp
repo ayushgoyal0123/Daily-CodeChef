65cb6004e03c0f8c4bd951bd448b1afed90912f8
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-12-05 20:35:47 +0530

    Add files via upload

---
+++ Day 63 NEARESTEXIT.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t)
+	{
+	    cin>>x;
+	    if(x<=50)
+	        cout<<"LEFT\n";
+	        
+	    else
+	        cout<<"RIGHT\n";
+	   t--;
+	}
+return 0;
+}
