06309e34eca99580d8251b753fd91ad457645a4e
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-12-15 23:39:37 +0530

    Add files via upload

---
+++ Day-73 C_RATING.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--)
+	{
+	     cin>>x>>y;
+	     cout<<ceil((y-x)/8.0)<<"\n";
+	}
+return 0;
+}
