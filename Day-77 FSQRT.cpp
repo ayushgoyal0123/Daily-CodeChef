47df832efc8efe9a2a8d6300278554f14ed6ab0a
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2024-12-19 20:37:33 +0530

    Add files via upload

---
+++ Day-77 FSQRT.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t,n;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        cout<<floor(sqrt(n))<<"\n";
+    }
+return 0;
+
+}
