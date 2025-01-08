d28225a0783ad664b723e1a7a0ec3bd0661ec0b6
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-01-08 23:20:32 +0530

    Add files via upload

---
+++ T20MCH.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int r,o,c;
+	scanf("%d%d%d",&r,&o,&c);
+	if((20-o)*36+c>r)
+	    printf("YES");
+	    
+	 else
+	    printf("NO");
+return 0;
+}
+
