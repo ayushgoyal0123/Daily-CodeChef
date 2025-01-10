6b44194796db26ef47afb32d1f1e48dee8875cf6
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-01-11 00:21:05 +0530

    Add files via upload

--- DICEGAME2.c
+++ DICEGAME2.c
@@ -1,49 +1,15 @@
 #include <stdio.h>
 
-int main(void) {
-	// your code goes here
-	int t,a,b,c,x,y,z,A,B,max;
-	scanf("%d",&t);
-	while(t--)
-	{
-	    scanf("%d%d%d%d%d%d",&a,&b,&c,&x,&y,&z);
-	    if(a>b && a>c)
-	    {
-	        max=(b>c)?b:c;
-	        A=a+max;
-	    }
-	    else if(b>c)
-	    {
-	        max=(c>a)?c:a;
-	        A=b+max;
-	    }
-	    else
-	    {
-	        max=(a>b)?a:b;
-	        A=c+max;
-	    }
-	    if(x>y && x>z)
-	    {
-	        max=(y>z)?y:z;
-	        B=x+max;
-	    }
-	    else if(y>z)
-	    {
-	        max=(z>x)?z:x;
-	        B=y+max;
-	    }
-	    else 
-	    {
-	        max=(x>y)?x:y;
-	        B=z+max;
-	    }
-	    if(A>B)
-	    printf("Alice\n");
-	    else if(B>A)
-	    printf("Bob\n");
-	    else 
-	    printf("Tie\n");
-	}
-	return 0;
+int main() {
+    int sum1, sum2, min1, min2, t, a1, a2, a3, b1, b2, b3;
+    scanf("%d", &t);
+    while (t--) {
+        scanf("%d%d%d%d%d%d", &a1, &a2, &a3, &b1, &b2, &b3);
+        min1=a1<a2&&a1<a3?a1:a2<a3?a2:a3;
+        min2=b1<b2&&b1<b3?b1:b2<b3?b2:b3;
+        sum1=min1==a1?a2+a3:min1==a2?a1+a3:a1+a2;
+        sum2=min2==b1?b2+b3:min2==b2?b1+b3:b1+b2;
+        printf(sum1>sum2?"Alice\n":sum1<sum2?"Bob\n":"Tie\n");
+    }
+    return 0;
 }
-
