4b5d49d3c056df4e76fe71d9c6dc48a0bf95caeb
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-01-20 23:59:47 +0530

    Add files via upload

---
+++ LUDO.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    printf(x==6?"YES\n":"NO\n");
+	}
+return 0;
+}
