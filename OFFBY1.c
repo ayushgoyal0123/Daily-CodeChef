be3a026cdd57139bfaa28a62fd577e3d9d38b0fb
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-01-25 23:55:33 +0530

    Add files via upload

---
+++ OFFBY1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+    short int a,b;
+	scanf("%d%d",&a,&b);
+	printf("%d",(a+b)*10+1);
+return 0;
+}
+
+
---
+++ OFFBY1.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	short int a,b;
+	cin>>a>>b;
+	cout<<(a+b)*10+1;
+return 0;
+}
