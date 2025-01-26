e4a8adf34c19af3e5c0a0082df9a179c4af06cb9
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-01-27 00:06:18 +0530

    Add files via upload

---
+++ DONDRIVE.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	short int t,x,n;
+	cin>>t;
+	while(t--){
+    cin>>n>>x;
+    cout<<n-x<<"\n";
+	}
+return 0;
+}
