2268d90b150eda80eb4d2c828ff2fb68c3f41707
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-01-29 23:59:01 +0530

    Add files via upload

---
+++ IPLTRSH.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {	
+    short int t;
+	int n,m;
+	cin>>t;
+	while(t--){
+	    cin>>n>>m;
+	    n-m<0?cout<<0<<"\n":cout<<n-m<<"\n";
+	}
+return 0;
+}
