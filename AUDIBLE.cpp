7ea247f75700103331a9fd227f227bb5e2660b92
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-01-30 23:52:19 +0530

    Add files via upload

---
+++ AUDIBLE.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    x>66 and x<45001? cout<<"YES\n" : cout<<"NO\n";
+	}
+return 0;
+}
