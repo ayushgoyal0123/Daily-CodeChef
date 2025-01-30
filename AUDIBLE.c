732d0e0f82fbd64339c833bfa463f9a28cf2dd31
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-01-30 23:57:42 +0530

    Add files via upload

---
+++ AUDIBLE.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+	// your code goes here
+	int t,x;
+	scanf("%d",&t);
+	while(t--){
+	    scanf("%d",&x);
+	    printf(x>66 &&  x<45001?"YES\n":"NO\n");
+	}
+return 0;
+}
