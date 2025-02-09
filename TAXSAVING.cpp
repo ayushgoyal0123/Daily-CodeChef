21183dc204c200dc096e38b0e38798ed2c52d8b8
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-02-09 23:56:31 +0530

    Add files via upload

---
+++ TAXSAVING.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	short int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y;
+	    cout<<x-y<<"\n";
+	 }
+return 0;
+}
