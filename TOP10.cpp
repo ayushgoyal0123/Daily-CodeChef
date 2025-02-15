170eea6791d1f4999fbc6cc3394dac5cf27ac8f7
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-02-15 23:48:05 +0530

    Add files via upload

---
+++ TOP10.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	short int t,x;
+	cin>>t;
+	while(t--){
+	    cin>>x;
+	    x<11?cout<<"YES\n": cout<<"NO\n";
+	}
+return 0;
+}
