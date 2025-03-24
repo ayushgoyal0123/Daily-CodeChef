f92fe60373a09da5f44290920d4584db6431d85c
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-03-25 00:06:07 +0530

    Add files via upload

---
+++ TALLER.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,x,y;
+	cin>>t;
+	while(t--){
+	    cin>>x>>y; 
+	    x>y?cout<<"A\n":cout<<"B\n";
+	}
+return 0;
+}
