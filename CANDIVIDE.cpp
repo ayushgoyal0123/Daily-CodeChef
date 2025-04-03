e0968a33a253d3442a689b442a5a1d5848407564
ayushgoyal0123 <136928400+ayushgoyal0123@users.noreply.github.com>
2025-04-04 00:06:34 +0530

    Add files via upload

---
+++ CANDIVIDE.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t,n;
+	cin>>t;
+	while(t--){
+	    cin>>n;
+	    n%3==0?cout<<"YES\n":cout<<"NO\n";
+	}
+return 0;
+}
---
+++ CANDIVIDE.py
@@ -0,0 +1,5 @@
+# cook your dish here
+t=int(input())
+for _ in range(t):
+    n= int(input())
+    print("YES" if n%3==0 else "NO")
